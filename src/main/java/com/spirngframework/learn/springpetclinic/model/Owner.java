017c299c967de69a8da79306e968c4952eecd321
Abhishek Kumar <Abhishek.kumar4@walmart.com>
2022-11-02 17:48:11 +0530

    feat: add model of per

---
+++ src/main/java/com/spirngframework/learn/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.spirngframework.learn.springpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/spirngframework/learn/springpetclinic/model/Pet.java
@@ -0,0 +1,11 @@
+package com.spirngframework.learn.springpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+
+}
