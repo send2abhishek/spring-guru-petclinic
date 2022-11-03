cd84a6b8c2ec11439571a511551aa410e42ec361
Abhishek Kumar <Abhishek.kumar4@walmart.com>
2022-11-03 17:50:45 +0530

    feat: create services interface

---
+++ pet-clinic-data/src/main/java/com/springframework/learn/springpetclinic/services/VetService.java
@@ -0,0 +1,15 @@
+package com.springframework.learn.springpetclinic.services;
+
+
+import com.springframework.learn.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
