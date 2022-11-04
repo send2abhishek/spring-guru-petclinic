16772862016d015f8b47055431a661bcfa5b080c
Abhishek Kumar <Abhishek.kumar4@walmart.com>
2022-11-04 16:37:02 +0530

    refactor: create CrudService

--- pet-clinic-data/src/main/java/com/springframework/learn/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/springframework/learn/springpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.springframework.learn.springpetclinic.services;
 
 import com.springframework.learn.springpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet,Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/springframework/learn/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/springframework/learn/springpetclinic/services/VetService.java
@@ -3,13 +3,9 @@ package com.springframework.learn.springpetclinic.services;
 
 import com.springframework.learn.springpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
 
-    Vet findById(Long id);
+public interface VetService extends CrudService<Vet, Long> {
 
-    Vet save(Vet vet);
 
-    Set<Vet> findAll();
 }
